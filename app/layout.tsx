29885b645a3325345369e0515946ade64eb6683b
MichaelTarigan1615 <umybestfriendchnty1615@gmail.com>
2026-07-16 17:35:07 +0700

    menambah status google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,13 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// UBAH BAGIAN INI: Ini adalah "Papan Nama" website Anda
+// UBAH BAGIAN INI: Ini adalah "Papan Nama" website Anda + Kode Verifikasi Google
 export const metadata: Metadata = {
   title: "Dashboard Kepling Kota Medan",
   description: "Dashboard Road to Universal Coverage Jamsostek untuk Kepala Lingkungan Kota Medan oleh BPJS Ketenagakerjaan.",
+  verification: {
+    google: "qYmI2FooNjsKHIVgygN-HAfITfWVMV0HkLk5ASb5yMA", // 💡 Kunci gembok verifikasi dari Google Search Console
+  },
 };
 
 export default function RootLayout({
